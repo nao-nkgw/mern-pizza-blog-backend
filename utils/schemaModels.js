c979a5896ddbd500ec7b447f62362830caa6037b
nao-nkgw <nao_nkgw@icloud.com>
2024-02-21 18:09:15 +0100

    changed schemaModels.js

--- utils/schemaModels.js
+++ utils/schemaModels.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const ItemSchema = new Schema({
   title: String,
   image: String,
-  star: Number,
+  star: String,
   rate: String,
   description: String,
   email: String,
